87a61cf23217a6250acedcb49c658484bf837424
Luis Zubia <soyzubia@gmail.com>
2022-12-21 16:40:15 -0600

    Add functionality to capitalize two words strings.

--- capitalize.js
+++ capitalize.js
@@ -6,7 +6,11 @@ function capitalize(string) {
   if (string.length === 1) return string.toUpperCase();
 
   if (string.includes(' ')) {
+    var words = string.split(' ');
 
+    words = words.map((word) => word[0].toUpperCase() + word.toLowerCase().slice(1));
+
+    return words.join(' ');
   }
 
   return string[0].toUpperCase() + string.toLowerCase().slice(1);
