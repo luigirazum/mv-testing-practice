51f5c2dd7108db16828b53dd19fcbc876c0cdc06
Luis Zubia <soyzubia@gmail.com>
2022-12-21 16:33:23 -0600

    Add functionality to thrown an error on invalid strings, and for one word strings.

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,15 @@
 function capitalize(string) {
-  return string.toUpperCase();
+  if (!string) {
+    throw new Error('Can\'t capitalize a null string!');
+  }
+
+  if (string.length === 1) return string.toUpperCase();
+
+  if (string.includes(' ')) {
+
+  }
+
+  return string[0].toUpperCase() + string.toLowerCase().slice(1);
 }
 
 module.exports = capitalize;
