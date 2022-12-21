5975fcf3fa278cd9e167dd314bf286f9cb4c051f
Luis Zubia <soyzubia@gmail.com>
2022-12-21 16:15:54 -0600

    Add tests for null and undefined values that should thrown an error by capitalize function.

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,40 @@
 const capitalize = require ('./capitalize');
 
+describe('Tests for non valid strings', () => {
+  test('empty string should thrown an error', () => {
+    // Arrange
+    const string = '';
+
+    // Act
+    const stringCapitalized = () => capitalize(string);
+
+    // Assert
+    expect(stringCapitalized).toThrow(Error);
+  });
+
+  test('null should thrown an error', () => {
+    // Arrange
+    const string = null;
+
+    // Act
+    const stringCapitalized = () => capitalize(string);
+
+    // Assert
+    expect(stringCapitalized).toThrow(Error);
+  });
+
+  test('undefined should thrown an error', () => {
+    // Arrange
+    const string = undefined;
+
+    // Act
+    const stringCapitalized = () => capitalize(string);
+
+    // Assert
+    expect(stringCapitalized).toThrow(Error);
+  });
+});
+
 describe('Tests for one word string', () => {
   test('string "a" should be returned as "A"', () => {
     // Arrange
